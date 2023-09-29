29a03b5421f6075a6025018082ecac2d41c1eca8
piyavach <111841685+piyavach@users.noreply.github.com>
2023-09-29 11:56:20 +0700

    not error

--- func_test1.js
+++ func_test1.js
@@ -1,7 +1,7 @@
 const {helloworld} = require('./utils')
 
 
-if(helloworld() === "hello worldx")
+if(helloworld() === "hello world")
 {
     console.log(  '{"_success":1, function_name:"helloworld()"}' );
 }else{
