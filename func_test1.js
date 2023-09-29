b9a5c3e89e74d69922245ac30b344e03ac2f2f4d
piyavach <111841685+piyavach@users.noreply.github.com>
2023-09-29 12:06:24 +0700

    update

--- func_test1.js
+++ func_test1.js
@@ -3,7 +3,6 @@ const {helloworld} = require('./utils')
 
 if(helloworld() === "hello world")
 {
-    console.log(  '{"_success":1, function_name:"helloworld()"}' );
 }else{
     console.log(  '1' );
 }
